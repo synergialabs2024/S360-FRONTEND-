f257a533f3c631c9ef8c1fbe092a3ed806ea29a4
Adrian <adrian.changalombo@espoch.edu.ec>
2024-08-20 20:54:43 +0000

    ADD: Flota interface

---
+++ src/shared/interfaces/app/mante-operacion/flota/flota.interface.ts
@@ -0,0 +1,39 @@
+import { PagingMetaResponse } from '@/shared/interfaces/common';
+
+export interface FlotasPaginatedRes {
+  status: number;
+  message: string;
+  meta: PagingMetaResponse;
+  items: Flota[];
+}
+
+export interface Flota {
+  id?: number;
+  uuid?: string;
+  state: boolean;
+
+  name: string;
+  marca_vehiculo: string;
+  modelo_vehiculo: string;
+  anio_vehiculo: number;
+  placa_vehiculo: string;
+  color_vehiculo: string;
+  email: string;
+  telefono_1: string;
+  telefono_2: string;
+  telefono_3: string;
+
+  created_at?: string;
+  modified_at?: string;
+
+  ///* fk
+  zonas?: number;
+  user?: number;
+  area?: number;
+  departamento?: number;
+  lider?: number;
+  auxiliar?: number;
+  pais?: number;
+  provincia?: number;
+  ciudad?: number;
+}
