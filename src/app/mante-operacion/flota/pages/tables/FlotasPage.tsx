9be64a94967f575b17b104d6d635945b334e9b7e
Adrian <adrian.changalombo@espoch.edu.ec>
2024-08-20 21:25:29 +0000

    ADD: useColumnsFlota

--- src/app/mante-operacion/flota/pages/tables/FlotasPage.tsx
+++ src/app/mante-operacion/flota/pages/tables/FlotasPage.tsx
@@ -1,20 +1,20 @@
-import { MRT_ColumnDef } from 'material-react-table';
-import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { useFetchFlotas, useUpdateFlota } from '@/actions/app';
+import { useFetchFlotas } from '@/actions/app';
 import { ROUTER_PATHS } from '@/router/constants';
 import {
   CustomSearch,
-  CustomSwitch,
   CustomTable,
   SingleTableBoxScene,
 } from '@/shared/components';
-import { MODEL_STATE_BOOLEAN, TABLE_CONSTANTS } from '@/shared/constants/ui';
-import { useTableFilter, useTableServerSideFiltering } from '@/shared/hooks';
+import { TABLE_CONSTANTS } from '@/shared/constants/ui';
+import {
+  useColumnsFlota,
+  useTableFilter,
+  useTableServerSideFiltering,
+} from '@/shared/hooks';
 import { useCheckPermission } from '@/shared/hooks/auth';
 import { Flota, PermissionsEnum } from '@/shared/interfaces';
-import { emptyCellOneLevel, formatDateWithTimeCell } from '@/shared/utils';
 import { hasAllPermissions, hasPermission } from '@/shared/utils/auth';
 import { useUiConfirmModalStore } from '@/store/ui';
 
@@ -37,11 +37,6 @@ const FlotasPage: React.FC<FlotasPageProps> = () => {
   const setConfirmDialogIsOpen = useUiConfirmModalStore(
     s => s.setConfirmDialogIsOpen,
   );
-
-  ///* mutations
-  const changeState = useUpdateFlota({
-    enableNavigate: false,
-  });
 
   ///* table
   const {
@@ -83,249 +78,7 @@ const FlotasPage: React.FC<FlotasPageProps> = () => {
   };
 
   ///* columns
-  const columns = useMemo<MRT_ColumnDef<Flota>[]>(
-    () => [
-      {
-        accessorKey: 'uuid',
-        header: 'UUID',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'uuid'),
-      },
-
-      {
-        accessorKey: 'state',
-        header: 'ESTADO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_SMALL,
-        enableSorting: false,
-        filterVariant: 'select',
-        filterSelectOptions: MODEL_STATE_BOOLEAN,
-        Cell: ({ row }) => {
-          return typeof row.original?.state === 'boolean' ? (
-            <CustomSwitch
-              title="state"
-              checked={row.original?.state}
-              onChangeChecked={() => {
-                if (
-                  !hasPermission(PermissionsEnum.mantenimientoope_change_flota)
-                )
-                  return;
-
-                setConfirmDialog({
-                  isOpen: true,
-                  title: 'Cambiar state',
-                  subtitle:
-                    '¿Está seguro que desea cambiar el state de este registro?',
-                  onConfirm: () => {
-                    changeState.mutate({
-                      id: row.original.id!,
-                      data: {
-                        state: !row.original.state,
-                      },
-                    });
-                    setConfirmDialogIsOpen(false);
-                  },
-                });
-              }}
-            />
-          ) : (
-            'N/A'
-          );
-        },
-      },
-
-      {
-        accessorKey: 'name',
-        header: 'NAME',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'name'),
-      },
-
-      {
-        accessorKey: 'marca_vehiculo',
-        header: 'MARCA VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'marca_vehiculo'),
-      },
-
-      {
-        accessorKey: 'modelo_vehiculo',
-        header: 'MODELO VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'modelo_vehiculo'),
-      },
-
-      {
-        accessorKey: 'anio_vehiculo',
-        header: 'ANIO VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'anio_vehiculo'),
-      },
-
-      {
-        accessorKey: 'placa_vehiculo',
-        header: 'PLACA VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'placa_vehiculo'),
-      },
-
-      {
-        accessorKey: 'color_vehiculo',
-        header: 'COLOR VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'color_vehiculo'),
-      },
-
-      {
-        accessorKey: 'email',
-        header: 'EMAIL',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'email'),
-      },
-
-      {
-        accessorKey: 'telefono_1',
-        header: 'TELEFONO 1',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_1'),
-      },
-
-      {
-        accessorKey: 'telefono_2',
-        header: 'TELEFONO 2',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_2'),
-      },
-
-      {
-        accessorKey: 'telefono_3',
-        header: 'TELEFONO 3',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_3'),
-      },
-
-      {
-        accessorKey: 'created_at',
-        header: 'CREADO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: false,
-        enableSorting: false,
-        Cell: ({ row }) => formatDateWithTimeCell(row, 'created_at'),
-      },
-
-      {
-        accessorKey: 'modified_at',
-        header: 'MODIFICADO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: false,
-        enableSorting: false,
-        Cell: ({ row }) => formatDateWithTimeCell(row, 'modified_at'),
-      },
-
-      {
-        accessorKey: 'zonas',
-        header: 'ZONAS',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'zonas'),
-      },
-
-      {
-        accessorKey: 'user',
-        header: 'USER',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'user'),
-      },
-
-      {
-        accessorKey: 'area',
-        header: 'AREA',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'area'),
-      },
-
-      {
-        accessorKey: 'departamento',
-        header: 'DEPARTAMENTO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'departamento'),
-      },
-
-      {
-        accessorKey: 'lider',
-        header: 'LIDER',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'lider'),
-      },
-
-      {
-        accessorKey: 'auxiliar',
-        header: 'AUXILIAR',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'auxiliar'),
-      },
-
-      {
-        accessorKey: 'pais',
-        header: 'PAIS',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'pais'),
-      },
-
-      {
-        accessorKey: 'provincia',
-        header: 'PROVINCIA',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'provincia'),
-      },
-
-      {
-        accessorKey: 'ciudad',
-        header: 'CIUDAD',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'ciudad'),
-      },
-    ],
-    [changeState, setConfirmDialog, setConfirmDialogIsOpen],
-  );
+  const { flotasColumns } = useColumnsFlota();
 
   return (
     <SingleTableBoxScene
@@ -340,7 +93,7 @@ const FlotasPage: React.FC<FlotasPageProps> = () => {
       />
 
       <CustomTable<Flota>
-        columns={columns}
+        columns={flotasColumns}
         data={FlotasPagingRes?.data?.items || []}
         isLoading={isLoading}
         isRefetching={isRefetching}
