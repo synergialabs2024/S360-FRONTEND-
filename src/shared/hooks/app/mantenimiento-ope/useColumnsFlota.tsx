b62a6ba53419b5f995aebcde19238f3365536afc
Adrian <adrian.changalombo@espoch.edu.ec>
2024-08-20 21:34:23 +0000

    ADD: useColumnsFlota v1.2

--- src/shared/hooks/app/mantenimiento-ope/useColumnsFlota.tsx
+++ src/shared/hooks/app/mantenimiento-ope/useColumnsFlota.tsx
@@ -5,7 +5,11 @@ import { useUpdateFlota } from '@/actions/app';
 import { CustomSwitch } from '@/shared/components';
 import { MODEL_STATE_BOOLEAN, TABLE_CONSTANTS } from '@/shared/constants';
 import { Flota, PermissionsEnum } from '@/shared/interfaces';
-import { emptyCellOneLevel, formatDateWithTimeCell } from '@/shared/utils';
+import {
+  emptyCellNested,
+  emptyCellOneLevel,
+  formatDateWithTimeCell,
+} from '@/shared/utils';
 import { hasPermission } from '@/shared/utils/auth';
 import { useUiConfirmModalStore } from '@/store/ui';
 
@@ -24,12 +28,165 @@ export const useColumnsFlota = () => {
   const flotasColumns = useMemo<MRT_ColumnDef<Flota>[]>(
     () => [
       {
-        accessorKey: 'uuid',
-        header: 'UUID',
+        accessorKey: 'name',
+        header: 'NAME',
         size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
         enableColumnFilter: true,
         enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'uuid'),
+        Cell: ({ row }) => emptyCellOneLevel(row, 'name'),
+      },
+
+      {
+        accessorKey: 'lider',
+        header: 'LIDER',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['lider_data', 'razon_social']),
+      },
+      {
+        accessorKey: 'auxiliar',
+        header: 'AUXILIAR',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) =>
+          emptyCellNested(row, ['auxiliar_data', 'razon_social']),
+      },
+
+      {
+        accessorKey: 'user',
+        header: 'USERNAME',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['user_data', 'username']),
+      },
+
+      {
+        accessorKey: 'marca_vehiculo',
+        header: 'MARCA VEHICULO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'marca_vehiculo'),
+      },
+      {
+        accessorKey: 'modelo_vehiculo',
+        header: 'MODELO VEHICULO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'modelo_vehiculo'),
+      },
+      {
+        accessorKey: 'anio_vehiculo',
+        header: 'ANIO VEHICULO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'anio_vehiculo'),
+      },
+      {
+        accessorKey: 'placa_vehiculo',
+        header: 'PLACA VEHICULO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'placa_vehiculo'),
+      },
+      {
+        accessorKey: 'color_vehiculo',
+        header: 'COLOR VEHICULO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'color_vehiculo'),
+      },
+
+      {
+        accessorKey: 'email',
+        header: 'EMAIL',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'email'),
+      },
+
+      {
+        accessorKey: 'telefono_1',
+        header: 'TELEFONO 1',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_1'),
+      },
+      {
+        accessorKey: 'telefono_2',
+        header: 'TELEFONO 2',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_2'),
+      },
+      {
+        accessorKey: 'telefono_3',
+        header: 'TELEFONO 3',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_3'),
+      },
+
+      {
+        accessorKey: 'area',
+        header: 'AREA',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['area_data', 'name']),
+      },
+      {
+        accessorKey: 'departamento',
+        header: 'DEPARTAMENTO',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['departamento_data', 'name']),
+      },
+
+      {
+        accessorKey: 'pais',
+        header: 'PAIS',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['pais_data', 'name']),
+      },
+      {
+        accessorKey: 'provincia',
+        header: 'PROVINCIA',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['provincia_data', 'name']),
+      },
+      {
+        accessorKey: 'ciudad',
+        header: 'CIUDAD',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellNested(row, ['ciudad_data', 'name']),
+      },
+      // TODO: modal polygon maps
+      {
+        accessorKey: 'zonas',
+        header: 'ZONAS',
+        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
+        enableColumnFilter: true,
+        enableSorting: true,
+        Cell: ({ row }) => emptyCellOneLevel(row, 'zonas'),
       },
 
       {
@@ -74,96 +231,6 @@ export const useColumnsFlota = () => {
       },
 
       {
-        accessorKey: 'name',
-        header: 'NAME',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'name'),
-      },
-
-      {
-        accessorKey: 'marca_vehiculo',
-        header: 'MARCA VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'marca_vehiculo'),
-      },
-
-      {
-        accessorKey: 'modelo_vehiculo',
-        header: 'MODELO VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'modelo_vehiculo'),
-      },
-
-      {
-        accessorKey: 'anio_vehiculo',
-        header: 'ANIO VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'anio_vehiculo'),
-      },
-
-      {
-        accessorKey: 'placa_vehiculo',
-        header: 'PLACA VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'placa_vehiculo'),
-      },
-
-      {
-        accessorKey: 'color_vehiculo',
-        header: 'COLOR VEHICULO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'color_vehiculo'),
-      },
-
-      {
-        accessorKey: 'email',
-        header: 'EMAIL',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'email'),
-      },
-
-      {
-        accessorKey: 'telefono_1',
-        header: 'TELEFONO 1',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_1'),
-      },
-
-      {
-        accessorKey: 'telefono_2',
-        header: 'TELEFONO 2',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_2'),
-      },
-
-      {
-        accessorKey: 'telefono_3',
-        header: 'TELEFONO 3',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'telefono_3'),
-      },
-
-      {
         accessorKey: 'created_at',
         header: 'CREADO',
         size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
@@ -171,7 +238,6 @@ export const useColumnsFlota = () => {
         enableSorting: false,
         Cell: ({ row }) => formatDateWithTimeCell(row, 'created_at'),
       },
-
       {
         accessorKey: 'modified_at',
         header: 'MODIFICADO',
@@ -179,87 +245,6 @@ export const useColumnsFlota = () => {
         enableColumnFilter: false,
         enableSorting: false,
         Cell: ({ row }) => formatDateWithTimeCell(row, 'modified_at'),
-      },
-
-      {
-        accessorKey: 'zonas',
-        header: 'ZONAS',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'zonas'),
-      },
-
-      {
-        accessorKey: 'user',
-        header: 'USER',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'user'),
-      },
-
-      {
-        accessorKey: 'area',
-        header: 'AREA',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'area'),
-      },
-
-      {
-        accessorKey: 'departamento',
-        header: 'DEPARTAMENTO',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'departamento'),
-      },
-
-      {
-        accessorKey: 'lider',
-        header: 'LIDER',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'lider'),
-      },
-
-      {
-        accessorKey: 'auxiliar',
-        header: 'AUXILIAR',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'auxiliar'),
-      },
-
-      {
-        accessorKey: 'pais',
-        header: 'PAIS',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'pais'),
-      },
-
-      {
-        accessorKey: 'provincia',
-        header: 'PROVINCIA',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'provincia'),
-      },
-
-      {
-        accessorKey: 'ciudad',
-        header: 'CIUDAD',
-        size: TABLE_CONSTANTS.COLUMN_WIDTH_MEDIUM,
-        enableColumnFilter: true,
-        enableSorting: true,
-        Cell: ({ row }) => emptyCellOneLevel(row, 'ciudad'),
       },
     ],
     [changeState, setConfirmDialog, setConfirmDialogIsOpen],
