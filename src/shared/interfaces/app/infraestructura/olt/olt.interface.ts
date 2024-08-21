eabea3ed2d7394370b19284558222fae7732131f
Adrian <adrian.changalombo@espoch.edu.ec>
2024-08-21 17:29:02 +0000

    ADD: OLT interface

---
+++ src/shared/interfaces/app/infraestructura/olt/olt.interface.ts
@@ -0,0 +1,38 @@
+import { PagingMetaResponse } from '@/shared/interfaces/common';
+
+export interface OltsPaginatedRes {
+  status: number;
+  message: string;
+  meta: PagingMetaResponse;
+  items: OLT;
+}
+
+export interface OLT {
+  id?: number;
+  uuid?: string;
+
+  state: boolean;
+
+  name: string;
+  direccion: string;
+  coordenadas: string;
+  descripcion: string;
+
+  puerto: string;
+  hostname: string;
+  password: string;
+  user: string;
+  pppoe: string;
+  ip_pppoe: string;
+
+  created_at?: string;
+  modified_at?: string;
+
+  ///* fk
+  nodo: number;
+  pais: number;
+  provincia: number;
+  ciudad: number;
+  zona: number;
+  sector: number;
+}
