2fe8cebba4a639a06b9ba5a7142c9ef94f0d04db
Douglas Coronado <84036785+Douglas-CO@users.noreply.github.com>
2024-09-26 10:11:45 -0500

    UPD: Actualizacion de CustonLineLoad con BackDrop (#59)

--- src/shared/components/Loaders/CustomLineLoad.tsx
+++ src/shared/components/Loaders/CustomLineLoad.tsx
@@ -1,35 +1,52 @@
-import { Box } from '@mui/material';
+import { Backdrop, Box } from '@mui/material';
 
 export type CustomLineLoadProps = {
   percentage?: number;
+  bDrop?: boolean;
 };
 
 const CustomLineLoad: React.FC<CustomLineLoadProps> = ({
   percentage = 100,
+  bDrop = false,
 }) => {
+  const iframeStyles = {
+    border: 'none',
+    clipPath: 'inset(25% 0 25% 0)',
+    width: '100%',
+    height: `${percentage}%`,
+  };
+
+  const containerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    clipPath: 'inset(25% 0 25% 0)',
+  };
+
   return (
     <>
-      <Box sx={{ display: 'flex' }}>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            clipPath: 'inset(25% 0 25% 0)',
-          }}
-        >
-          <iframe
-            src="https://lottie.host/embed/5c811360-d048-43b4-8817-3d59bc8e2537/guIFnSbYCI.json"
-            style={{
-              border: 'none',
-              clipPath: 'inset(25% 0 25% 0)',
-              width: '100%',
-              height: `${percentage}%`,
-            }}
-          ></iframe>
+      {bDrop ? (
+        <Backdrop sx={{ zIndex: 10001 }} open={true}>
+          <Box sx={{ display: 'flex' }}>
+            <Box sx={containerStyles}>
+              <iframe
+                src="https://lottie.host/embed/5c811360-d048-43b4-8817-3d59bc8e2537/guIFnSbYCI.json"
+                style={iframeStyles}
+              />
+            </Box>
+          </Box>
+        </Backdrop>
+      ) : (
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={containerStyles}>
+            <iframe
+              src="https://lottie.host/embed/5c811360-d048-43b4-8817-3d59bc8e2537/guIFnSbYCI.json"
+              style={iframeStyles}
+            />
+          </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 };
