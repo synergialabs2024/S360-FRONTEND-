7307f7c60ce674c67980cf4b5ad381d1aa1fc77d
Adrian <adrian.changalombo@espoch.edu.ec>
2024-09-27 20:37:49 +0000

    ADD: GeneralDataConfirmAgendaStep with nested tabs

--- src/app/operaciones/agedamiento/shared/components/form/GeneralDataConfirmAgendaStep.tsx
+++ src/app/operaciones/agedamiento/shared/components/form/GeneralDataConfirmAgendaStep.tsx
@@ -4,15 +4,21 @@ import {
   Agendamiento,
   gridSizeMdLg6,
   PARENTESCO_TYPE_ARRAY_CHOICES,
+  useTabsOnly,
 } from '@/shared';
 import {
+  a11yProps,
   CustomCellphoneTextField,
+  CustomTabPanel,
   CustomTextField,
   CustomTextFieldNoForm,
   CustomTypoLabel,
   CustomTypoLabelEnum,
+  FormTabsOnly,
+  NestedTabsScene,
   SelectTextFieldArrayString,
 } from '@/shared/components';
+import { Tab } from '@mui/material';
 import type { SaveConfirmAgendaOperaciones } from '../SaveConfirmAgendaOperaciones';
 import LocationZonePolygonFormPart from './LocationZonePolygonFormPart';
 
@@ -24,85 +30,101 @@ export type GeneralDataConfirmAgendaStepProps = {
 const GeneralDataConfirmAgendaStep: React.FC<
   GeneralDataConfirmAgendaStepProps
 > = ({ form, agendamiento }) => {
+  ///* hooks ---------------------
+  const { tabValue, handleTabChange } = useTabsOnly();
+
   ///* form ---------------------
   const { errors } = form.formState;
 
   return (
     <>
-      <CustomTypoLabel text="Datos Cliente" />
+      <NestedTabsScene
+        tabs={
+          <FormTabsOnly value={tabValue} onChange={handleTabChange}>
+            <Tab label="Datos Cliente" value={1} {...a11yProps(1)} />
+            <Tab label="Ubicadión Cliente" value={2} {...a11yProps(2)} />
+          </FormTabsOnly>
+        }
+        sxContainer={{
+          pt: 0,
+          pb: 0,
+        }}
+      >
+        <CustomTabPanel value={tabValue} index={1} ptGrid="0">
+          <CustomTextField
+            label="Tipo identificación"
+            name="tipo_identificacion"
+            control={form.control}
+            defaultValue={form.getValues().tipo_identificacion}
+            error={errors.tipo_identificacion}
+            helperText={errors.tipo_identificacion?.message}
+            disabled
+            size={gridSizeMdLg6}
+          />
+          <CustomTextField
+            label="Identificación"
+            name="identificacion"
+            control={form.control}
+            defaultValue={form.getValues().identificacion}
+            error={errors.identificacion}
+            helperText={errors.identificacion?.message}
+            disabled
+            size={gridSizeMdLg6}
+          />
+          <CustomTextField
+            label="Razon social"
+            name="razon_social"
+            control={form.control}
+            defaultValue={form.getValues().razon_social}
+            error={errors.razon_social}
+            helperText={errors.razon_social?.message}
+            disabled
+          />
 
-      <CustomTextField
-        label="Tipo identificación"
-        name="tipo_identificacion"
-        control={form.control}
-        defaultValue={form.getValues().tipo_identificacion}
-        error={errors.tipo_identificacion}
-        helperText={errors.tipo_identificacion?.message}
-        disabled
-        size={gridSizeMdLg6}
-      />
-      <CustomTextField
-        label="Identificación"
-        name="identificacion"
-        control={form.control}
-        defaultValue={form.getValues().identificacion}
-        error={errors.identificacion}
-        helperText={errors.identificacion?.message}
-        disabled
-        size={gridSizeMdLg6}
-      />
-      <CustomTextField
-        label="Razon social"
-        name="razon_social"
-        control={form.control}
-        defaultValue={form.getValues().razon_social}
-        error={errors.razon_social}
-        helperText={errors.razon_social?.message}
-        disabled
-      />
+          {/* =================== Persona Referencia =================== */}
+          <CustomTextField
+            label="Nombre Persona Referencia"
+            name="nombre_persona_referencia"
+            control={form.control}
+            defaultValue={form.getValues().nombre_persona_referencia}
+            error={errors.nombre_persona_referencia}
+            helperText={errors.nombre_persona_referencia?.message}
+          />
+          <SelectTextFieldArrayString
+            label="Parentesco Referencia"
+            name="parentesco_referencia"
+            textFieldKey="parentesco_referencia"
+            // options
+            options={PARENTESCO_TYPE_ARRAY_CHOICES}
+            defaultValue={form.getValues()?.parentesco_referencia || ''}
+            // errors
+            control={form.control}
+            error={form.formState.errors.parentesco_referencia}
+            helperText={form.formState.errors.parentesco_referencia?.message}
+            gridSize={gridSizeMdLg6}
+          />
+          <CustomCellphoneTextField
+            label="Celular Referencia"
+            name="celular_adicional"
+            control={form.control}
+            defaultValue={form.getValues().celular_adicional}
+            error={errors.celular_adicional}
+            helperText={errors.celular_adicional?.message}
+            size={gridSizeMdLg6}
+          />
+        </CustomTabPanel>
 
-      {/* =================== Persona Referencia =================== */}
-      <CustomTextField
-        label="Nombre Persona Referencia"
-        name="nombre_persona_referencia"
-        control={form.control}
-        defaultValue={form.getValues().nombre_persona_referencia}
-        error={errors.nombre_persona_referencia}
-        helperText={errors.nombre_persona_referencia?.message}
-      />
-      <SelectTextFieldArrayString
-        label="Parentesco Referencia"
-        name="parentesco_referencia"
-        textFieldKey="parentesco_referencia"
-        // options
-        options={PARENTESCO_TYPE_ARRAY_CHOICES}
-        defaultValue={form.getValues()?.parentesco_referencia || ''}
-        // errors
-        control={form.control}
-        error={form.formState.errors.parentesco_referencia}
-        helperText={form.formState.errors.parentesco_referencia?.message}
-        gridSize={gridSizeMdLg6}
-      />
-      <CustomCellphoneTextField
-        label="Celular Referencia"
-        name="celular_adicional"
-        control={form.control}
-        defaultValue={form.getValues().celular_adicional}
-        error={errors.celular_adicional}
-        helperText={errors.celular_adicional?.message}
-        size={gridSizeMdLg6}
-      />
-
-      {/* =================== Ubicación =================== */}
-      <>
-        <LocationZonePolygonFormPart
-          form={form}
-          initialCoords={
-            agendamiento?.solicitud_servicio_data?.coordenadas || ''
-          }
-          isEdit={true}
-        />
-      </>
+        <CustomTabPanel value={tabValue} index={2} ptGrid="0">
+          {/* =================== Ubicación =================== */}
+          <LocationZonePolygonFormPart
+            form={form}
+            initialCoords={
+              agendamiento?.solicitud_servicio_data?.coordenadas || ''
+            }
+            isEdit={true}
+          />
+        </CustomTabPanel>
+      </NestedTabsScene>
 
       {/* =================== Datos Vendedor =================== */}
       <>
