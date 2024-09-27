8389caa11e157315fe7363c9f38470c622b2602a
Jeanyiga5 <jean.moreano@intercommerce.com.ec>
2024-09-27 21:22:39 +0000

    no devploy commit

--- src/app/comercial/agendamiento/shared/components/SaveAgendamiento/SaveAgendamiento.tsx
+++ src/app/comercial/agendamiento/shared/components/SaveAgendamiento/SaveAgendamiento.tsx
@@ -55,7 +55,7 @@ const SaveAgendamiento: React.FC<SaveAgendamientoProps> = ({
   const { activeStep, disableNextStepBtn, handleBack, handleNext } =
     useCustomStepper({
       steps,
-      initialStep: 2,
+      initialStep: 0,
     });
 
   ///* global state ---------------------
