3e7cd76da3933f7ac61719cadabaa80fe93d5737
SLian05 <lian.mejia@synergialabs.com.ec>
2024-10-17 16:36:19 +0000

    REMOVE: Se removio el .env del repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
-import MillionLint from '@million/lint';
 import path from 'path';
+import million from 'million/compiler';
 import svgr from '@svgr/rollup';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [MillionLint.vite(),,svgr(), react()],
+  plugins: [million.vite({ auto: true }), svgr(), react()],
 
   resolve: {
     alias: {
