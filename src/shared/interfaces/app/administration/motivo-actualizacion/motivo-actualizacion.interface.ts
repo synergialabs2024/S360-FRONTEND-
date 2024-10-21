d16e13ee85fba78aa28ee4fb187a3ddb78505b0c
Adrian <adrian.changalombo@espoch.edu.ec>
2024-10-21 20:21:16 +0000

    ADD: MotivoActualizacion interface

---
+++ src/shared/interfaces/app/administration/motivo-actualizacion/motivo-actualizacion.interface.ts
@@ -0,0 +1,21 @@
+import { PagingMetaResponse } from '@/shared/interfaces/common';
+
+export interface MotivosActualizacionPaginatedRes {
+  status: number;
+  message: string;
+  meta: PagingMetaResponse;
+  items: MotivoActualizacion[];
+}
+
+export interface MotivoActualizacion {
+  id?: number;
+  uuid?: string;
+
+  name: string;
+  description: string;
+  state: boolean;
+  modulo: string;
+
+  created_at?: string;
+  modified_at?: string;
+}
