8cc9cae591eb4fba5a6c22c1d6f42ecd07bc59bb
Adrian <adrian.changalombo@espoch.edu.ec>
2024-11-12 20:44:12 +0000

    ADD: ClienteFibraOTFotosPart

--- src/app/cliente/cliente/shared/components/fibra/servicio/ClienteFibraOTFotosPart.tsx
+++ src/app/cliente/cliente/shared/components/fibra/servicio/ClienteFibraOTFotosPart.tsx
@@ -1,4 +1,5 @@
-import { LineaServicio } from '@/shared';
+import { calcImageListOrdenTrabajoFromUrls, LineaServicio } from '@/shared';
+import { ImageListTitleBars } from '@/shared/components/ui';
 
 export type ClienteFibraOTFotosPartProps = {
   serviceLine: LineaServicio;
@@ -7,8 +8,13 @@ export type ClienteFibraOTFotosPartProps = {
 const ClienteFibraOTFotosPart: React.FC<ClienteFibraOTFotosPartProps> = ({
   serviceLine,
 }) => {
-  console.log(serviceLine);
-  return <>ClienteFibraOTFotosPart</>;
+  return (
+    <ImageListTitleBars
+      images={
+        calcImageListOrdenTrabajoFromUrls(serviceLine?.orden_trabajo_data) || []
+      }
+    />
+  );
 };
 
 export default ClienteFibraOTFotosPart;
--- src/shared/utils/images/calc-images-url.util.ts
+++ src/shared/utils/images/calc-images-url.util.ts
@@ -1,4 +1,5 @@
-import { Preventa } from '@/shared';
+/* eslint-disable indent */
+import type { OrdenTrabajo, Preventa } from '@/shared';
 import { ImageListTitleBarType } from '@/shared/components/ui';
 
 export const calcImageListPreventaFromUrls = (
@@ -16,57 +17,157 @@ export const calcImageListPreventaFromUrls = (
   return [
     ...(fotosUrls[0]
       ? [
-        {
-          title: 'Frontal identificación',
-          imgUrl: fotosUrls[0],
-          id: 0,
-        },
-      ]
+          {
+            title: 'Frontal identificación',
+            imgUrl: fotosUrls[0],
+            id: 0,
+          },
+        ]
       : []),
     ...(fotosUrls[1]
       ? [
-        {
-          title: 'Reverso identificación',
-          imgUrl: fotosUrls[1],
-          id: 1,
-        },
-      ]
+          {
+            title: 'Reverso identificación',
+            imgUrl: fotosUrls[1],
+            id: 1,
+          },
+        ]
       : []),
     ...(fotosUrls[2]
       ? [
-        {
-          title: 'Vivienda',
-          imgUrl: fotosUrls[2],
-          id: 2,
-        },
-      ]
+          {
+            title: 'Vivienda',
+            imgUrl: fotosUrls[2],
+            id: 2,
+          },
+        ]
       : []),
     ...(fotosUrls[3]
       ? [
-        {
-          title: 'Aceptación servicio',
-          imgUrl: fotosUrls[3],
-          id: 3,
-        },
-      ]
+          {
+            title: 'Aceptación servicio',
+            imgUrl: fotosUrls[3],
+            id: 3,
+          },
+        ]
       : []),
     ...(fotosUrls[4]
       ? [
-        {
-          title: 'Documento cuenta',
-          imgUrl: fotosUrls[4],
-          id: 4,
-        },
-      ]
+          {
+            title: 'Documento cuenta',
+            imgUrl: fotosUrls[4],
+            id: 4,
+          },
+        ]
       : []),
     ...(fotosUrls[5]
       ? [
-        {
-          title: 'Tarjeta',
-          imgUrl: fotosUrls[5],
-          id: 5,
-        },
-      ]
+          {
+            title: 'Tarjeta',
+            imgUrl: fotosUrls[5],
+            id: 5,
+          },
+        ]
+      : []),
+  ];
+};
+
+export const calcImageListOrdenTrabajoFromUrls = (
+  ordenTrabajo?: OrdenTrabajo,
+): ImageListTitleBarType[] => {
+  const fotosUrls = [
+    ordenTrabajo?.url_foto_ont,
+    ordenTrabajo?.url_foto_potencia_ont,
+    ordenTrabajo?.url_foto_ont_encontrado_casa,
+    ordenTrabajo?.url_foto_etiqueta,
+    ordenTrabajo?.url_foto_nap,
+    ordenTrabajo?.url_foto_potencia_nap,
+    ordenTrabajo?.url_foto_premio,
+    ordenTrabajo?.url_foto_test_speed,
+    ordenTrabajo?.url_foto_acta_entrega_ups,
+  ];
+
+  return [
+    ...(fotosUrls[0]
+      ? [
+          {
+            title: 'ONT',
+            imgUrl: fotosUrls[0],
+            id: 0,
+          },
+        ]
+      : []),
+    ...(fotosUrls[1]
+      ? [
+          {
+            title: 'Potencia ONT',
+            imgUrl: fotosUrls[1],
+            id: 1,
+          },
+        ]
+      : []),
+    ...(fotosUrls[2]
+      ? [
+          {
+            title: 'ONT encontrado en casa',
+            imgUrl: fotosUrls[2],
+            id: 2,
+          },
+        ]
+      : []),
+    ...(fotosUrls[3]
+      ? [
+          {
+            title: 'Etiqueta',
+            imgUrl: fotosUrls[3],
+            id: 3,
+          },
+        ]
+      : []),
+    ...(fotosUrls[4]
+      ? [
+          {
+            title: 'NAP',
+            imgUrl: fotosUrls[4],
+            id: 4,
+          },
+        ]
+      : []),
+    ...(fotosUrls[5]
+      ? [
+          {
+            title: 'Potencia NAP',
+            imgUrl: fotosUrls[5],
+            id: 5,
+          },
+        ]
+      : []),
+    ...(fotosUrls[6]
+      ? [
+          {
+            title: 'Premio',
+            imgUrl: fotosUrls[6],
+            id: 6,
+          },
+        ]
+      : []),
+    ...(fotosUrls[7]
+      ? [
+          {
+            title: 'Test speed',
+            imgUrl: fotosUrls[7],
+            id: 7,
+          },
+        ]
+      : []),
+    ...(fotosUrls[8]
+      ? [
+          {
+            title: 'Acta entrega UPS',
+            imgUrl: fotosUrls[8],
+            id: 8,
+          },
+        ]
       : []),
   ];
 };
