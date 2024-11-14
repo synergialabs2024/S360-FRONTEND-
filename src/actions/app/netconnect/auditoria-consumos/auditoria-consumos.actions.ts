122978268750090d0f9f24406c9980e652f37ad2
Douglas Coronado <84036785+Douglas-CO@users.noreply.github.com>
2024-11-14 10:37:53 -0500

    UPD: Update Model Auditoria Consumo (#22)

--- src/actions/app/netconnect/auditoria-consumos/auditoria-consumos.actions.ts
+++ src/actions/app/netconnect/auditoria-consumos/auditoria-consumos.actions.ts
@@ -4,15 +4,64 @@ import { useQuery } from '@tanstack/react-query';
 import { handleAxiosError } from '@/shared/axios/axios.utils';
 
 export enum AuditoriaConsumosONUTSQEnum {
-  AUDITORIACONSUMOS = 'auditoria-consumos',
-  AUDITORIACONSUMO = 'auditoria-consumo',
+  CLIENTESUSPENDIDOSCONSUMOS = 'cliente-suspendidos-consumos',
+  CLIENTEACTIVOSALTOCONSUMOS = 'cliente-activos-alto-consumos',
+  CLIENTEACTIVOSMOROSOS = 'cliente-activos-morosos',
+  CLIENTESUSPENDIDOSCONSUMOSMK = 'cliente-suspendidos-consumos-mks',
 }
 
-const API_DATA = '';
+const API_DATA_CLIENTES_SUSPENDIDOS_CONSUMO = '';
+const API_DATA_CLIENTES_ACTIVOS_ALTO_CONSUMO = '';
+const API_DATA_CLIENTES_ACTIVOS_MOROSO = '';
+const API_DATA_CLIENTES_SUSPENDIDOS_CONSUMO_MK = '';
 
-export const fetchCombinedDataAuditoriaConsumo = async (params?: any) => {
+export const fetchCombinedDataClientesSuspendidosConsumo = async (
+  params?: any,
+) => {
   try {
-    const response = await axios.get(API_DATA, { params });
+    const response = await axios.get(API_DATA_CLIENTES_SUSPENDIDOS_CONSUMO, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    handleAxiosError(error);
+    throw error;
+  }
+};
+
+export const fetchCombinedDataClientesActivosAltoConsumo = async (
+  params?: any,
+) => {
+  try {
+    const response = await axios.get(API_DATA_CLIENTES_ACTIVOS_ALTO_CONSUMO, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    handleAxiosError(error);
+    throw error;
+  }
+};
+
+export const fetchCombinedDataClientesActivosMoroso = async (params?: any) => {
+  try {
+    const response = await axios.get(API_DATA_CLIENTES_SUSPENDIDOS_CONSUMO_MK, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    handleAxiosError(error);
+    throw error;
+  }
+};
+
+export const fetchCombinedDataClientesSuspendidosConsumoMK = async (
+  params?: any,
+) => {
+  try {
+    const response = await axios.get(API_DATA_CLIENTES_ACTIVOS_MOROSO, {
+      params,
+    });
     return response.data;
   } catch (error) {
     handleAxiosError(error);
@@ -21,7 +70,7 @@ export const fetchCombinedDataAuditoriaConsumo = async (params?: any) => {
 };
 
 ///* tanStack query ---------------
-export const useFetchAuditoriaConsumos = ({
+export const useFetchClientesSuspendidosConsumo = ({
   enabled = true,
   params,
 }: {
@@ -30,10 +79,61 @@ export const useFetchAuditoriaConsumos = ({
 }) => {
   return useQuery({
     queryKey: [
-      AuditoriaConsumosONUTSQEnum.AUDITORIACONSUMOS,
+      AuditoriaConsumosONUTSQEnum.CLIENTESUSPENDIDOSCONSUMOS,
       ...Object.values(params || {}),
     ],
-    queryFn: () => fetchCombinedDataAuditoriaConsumo(params),
+    queryFn: () => fetchCombinedDataClientesSuspendidosConsumo(params),
+    enabled: enabled,
+  });
+};
+
+export const useFetchClientesActivosAltoConsumo = ({
+  enabled = true,
+  params,
+}: {
+  enabled?: boolean;
+  params?: any;
+}) => {
+  return useQuery({
+    queryKey: [
+      AuditoriaConsumosONUTSQEnum.CLIENTEACTIVOSALTOCONSUMOS,
+      ...Object.values(params || {}),
+    ],
+    queryFn: () => fetchCombinedDataClientesActivosAltoConsumo(params),
+    enabled: enabled,
+  });
+};
+
+export const useFetchClientesActivosMoroso = ({
+  enabled = true,
+  params,
+}: {
+  enabled?: boolean;
+  params?: any;
+}) => {
+  return useQuery({
+    queryKey: [
+      AuditoriaConsumosONUTSQEnum.CLIENTEACTIVOSMOROSOS,
+      ...Object.values(params || {}),
+    ],
+    queryFn: () => fetchCombinedDataClientesActivosMoroso(params),
+    enabled: enabled,
+  });
+};
+
+export const useFetchClientesSuspendidosConsumoMK = ({
+  enabled = true,
+  params,
+}: {
+  enabled?: boolean;
+  params?: any;
+}) => {
+  return useQuery({
+    queryKey: [
+      AuditoriaConsumosONUTSQEnum.CLIENTESUSPENDIDOSCONSUMOSMK,
+      ...Object.values(params || {}),
+    ],
+    queryFn: () => fetchCombinedDataClientesSuspendidosConsumoMK(params),
     enabled: enabled,
   });
 };
