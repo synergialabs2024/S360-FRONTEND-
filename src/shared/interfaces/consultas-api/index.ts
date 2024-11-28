ba1d3afd5874cd2835e130f2b6caf9519a70265e
Adrian <adrian.changalombo@espoch.edu.ec>
2024-11-28 15:14:50 +0000

    ADD: PersonaInformacion

--- src/shared/interfaces/consultas-api/index.ts
+++ src/shared/interfaces/consultas-api/index.ts
@@ -1,2 +1,3 @@
 export * from './cedula-citizen.interface';
 export * from './equifax-servicio-cedula.interface';
+export * from './persona-informacion.interface';
---
+++ src/shared/interfaces/consultas-api/persona-informacion.interface.ts
@@ -0,0 +1,59 @@
+export interface PersonaInformacion {
+  id: string;
+  nombres: string;
+  defuncion: null;
+  ci: string;
+  email: string;
+  ciudad: string;
+  provincia: string;
+  nacimiento: Date;
+  estadocivil: string;
+  idCliente: string;
+  sexo: string;
+  direccion: string;
+  telefono: string;
+  movil: null;
+  info_adicional?: null;
+  Informacion?: null;
+  laboral?: Laboral[];
+  Buro?: Buro[];
+  Family?: Family[];
+  Credit?: null;
+  contactos?: Contacto[];
+}
+
+export interface Buro {
+  fecha: Date;
+  riesgo: string;
+  calificacion: string;
+  saldo_vigente: string;
+  mora: string;
+  saldo_mora: string;
+  nom_banco: string;
+}
+
+export interface Family {
+  nombres: string;
+  ci: string;
+  relacion: string;
+  id_call_tipo_relacion: string;
+  sexo: string;
+  fecha: null;
+  nacimiento: Date;
+  id: string;
+  defuncion: null;
+}
+
+export interface Contacto {
+  contacto: string;
+  tipo: string;
+  estado: string;
+}
+
+export interface Laboral {
+  ingreso: Date;
+  salario: string;
+  empresa: string;
+  ocupacion: string;
+  salida: string;
+}
