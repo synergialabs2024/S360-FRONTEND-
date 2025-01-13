fb04a421bee3b9432b601cc7b9f93cdcb2ccf885
Adrian <adrian.changalombo@espoch.edu.ec>
2025-01-13 21:50:56 +0000

    FIX: some build problems

--- src/_mockApis/index.ts
+++ src/_mockApis/index.ts
@@ -1,7 +1,5 @@
 import mock from './mock';
 import './notes/NotesData';
-import './ticket/TicketData';
-import './eCommerce/ProductsData';
 import './invoice/invoceLists';
 
 mock.onAny().passThrough();
