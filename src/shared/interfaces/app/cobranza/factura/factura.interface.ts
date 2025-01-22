dbc586b6c8c0769201f6894114add7f197691a6c
Adrian <adrian.changalombo@espoch.edu.ec>
2025-01-22 15:53:28 +0000

    ADD: Factura interface

---
+++ src/shared/interfaces/app/cobranza/factura/factura.interface.ts
@@ -0,0 +1,25 @@
+import { PagingMetaResponse } from '@/shared/interfaces/common';
+
+export interface FacturasPaginatedRes {
+  status: number;
+  message: string;
+  meta: PagingMetaResponse;
+  items: Factura[];
+}
+
+export interface Factura {
+  id: number;
+  uuid: string;
+
+  fecha_emision: string;
+
+  url_pdf: string;
+  url_xml: string;
+
+  total: string;
+  clave_acceso: string;
+  numero: string;
+
+  created_at: string;
+  modified_at: string;
+}
