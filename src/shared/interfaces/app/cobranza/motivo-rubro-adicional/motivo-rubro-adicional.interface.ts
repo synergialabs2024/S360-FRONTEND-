0cb4b8ca63659dde0dc87e0747749b1581da828e
Adrian <adrian.changalombo@espoch.edu.ec>
2025-01-27 20:33:44 +0000

    ADD: motivo rubro adicional interface

---
+++ src/shared/interfaces/app/cobranza/motivo-rubro-adicional/motivo-rubro-adicional.interface.ts
@@ -0,0 +1,21 @@
+import { PagingMetaResponse } from '@/shared/interfaces/common';
+
+export interface MotivosRubroAdicionalPaginatedRes {
+  status: number;
+  message: string;
+  meta: PagingMetaResponse;
+  items: MotivoRubroAdicional[];
+}
+
+export interface MotivoRubroAdicional {
+  id?: number;
+  uuid?: string;
+
+  nombre: string;
+  codigo: string;
+  valor: string;
+  descripcion?: string;
+
+  created_at?: string;
+  modified_at?: string;
+}
